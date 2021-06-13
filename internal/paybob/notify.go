203e72a47fd0281d75c1d503fa3bc22c88c5d84d
E99p1ant <i@github.red>
2021-06-13 15:19:46 +0800

    paybob: parse notify request body

--- internal/paybob/notify.go
+++ internal/paybob/notify.go
@@ -3,3 +3,28 @@
 // license that can be found in the LICENSE file.
 
 package paybob
+
+import (
+	"net/http"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+type NotifyResponse struct {
+	ReturnCode    int    `json:"return_code"`
+	TotalFee      int    `json:"total_fee"`
+	OutTradeNo    string `json:"out_trade_no"`
+	PayJSOrderID  string `json:"payjs_order_id"`
+	TransactionID string `json:"transaction_id"`
+	TimeEnd       string `json:"time_end"`
+	OpenID        string `json:"open_id"`
+	Attach        string `json:"attach"`
+	MchID         string `json:"mchid"`
+	Sign          string `json:"sign"`
+}
+
+// ParseNotify parse the notify request body.
+func (c *Client) ParseNotify(req *http.Request) (*NotifyResponse, error) {
+	var requestBody NotifyResponse
+	return &requestBody, jsoniter.NewDecoder(req.Body).Decode(&requestBody)
+}
