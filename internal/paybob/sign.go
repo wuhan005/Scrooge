854c9afc689e0e720656ba7c974e340195ff563d
E99p1ant <i@github.red>
2021-06-19 14:02:08 +0800

    paybob: fix sign

--- internal/paybob/sign.go
+++ internal/paybob/sign.go
@@ -7,11 +7,40 @@ package paybob
 import (
 	"fmt"
 	"net/url"
+	"sort"
 	"strings"
 
 	"github.com/wuhan005/gadget"
 )
 
 func (c *Client) Sign(query url.Values) string {
-	return strings.ToUpper(gadget.Md5(fmt.Sprintf("%s&key=%s", query.Encode(), c.key)))
+	if query == nil {
+		return ""
+	}
+	// Remove the empty value.
+	for k, v := range query {
+		if len(v) == 0 || v[0] == "" {
+			query.Del(k)
+		}
+	}
+
+	var buf strings.Builder
+	keys := make([]string, 0, len(query))
+	for k := range query {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	for _, k := range keys {
+		vs := query[k]
+		for _, v := range vs {
+			if buf.Len() > 0 {
+				buf.WriteByte('&')
+			}
+			buf.WriteString(k)
+			buf.WriteByte('=')
+			buf.WriteString(v)
+		}
+	}
+
+	return strings.ToUpper(gadget.Md5(fmt.Sprintf("%s&key=%s", buf.String(), c.key)))
 }
